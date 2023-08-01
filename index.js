28e40c7a2c773e4b1269b15d9f2f1d483061eb55
Bombata Habeeb <bombatahabeeb@bombatas-mbp.home>
2023-08-02 00:46:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser"
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const password = "ILoveProgramming"
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", (req, res) =>{
+    req.body
+    res.sendFile(__dirname + "/public/index.html")
+    });
+    
+
+app.post("/check", (req, res) =>{
+           if (password == req.body["password"]){
+            res.sendFile(__dirname + "/public/secret.html")
+           } else {
+            res.send(`<h1> Wrong Password </h1>`)
+           }
+        });
+  
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  
+  });
+  
+
